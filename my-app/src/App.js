84f05019a1b5964e0a55d383e9187aa30a60a77c
migjesus <migjesus21@gmail.com>
2021-11-10 10:53:48 +0000

    deleted unused dependencies and optimized code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,104 +2,122 @@ import "./App.css";
 import ml5 from "ml5";
 import Sketch from "react-p5";
 import mostCommon from "array-most-common";
+import { constants } from "./constants";
+
+const {
+  TOTAL_GAME_TIME,
+  VIDEO_HEIGHT,
+  VIDEO_WIDTH,
+  POSSIBLE_POSES,
+  STATE: { WAITING, COLLECTING },
+} = constants;
+
+const VIDEO_SETTINGS = {
+  video: {
+    audio: false,
+    height: VIDEO_HEIGHT,
+    width: VIDEO_WIDTH,
+  },
+};
 
 const App = () => {
   let video;
   let poseNet;
   let pose;
   let skeleton;
-  const videoConstraints = {
-    video: {
-      audio: false,
-      height: 240,
-      width: 320,
-    },
-  };
-  let neurons;
-  let state = "waiting";
+  let neuralNetwork;
+  let state = WAITING;
   let targetLabel;
   let sequence = [];
   let randomPose = "e";
-  let possiblePoses = ["a", "b"];
   let cool = false;
   let running = false;
-  const totalGameTime = 30;
-  let timer = totalGameTime;
+  let timer = TOTAL_GAME_TIME;
   let movementCount = 0;
   let debugMode = false;
 
   const keyPressed = (event) => {
     if (event.key === "s") {
-      neurons.saveData();
+      neuralNetwork.saveData();
     } else {
       targetLabel = event.key;
       console.log(targetLabel);
       setTimeout(() => {
         console.log("collecting");
-        state = "collecting";
+        state = COLLECTING;
         setTimeout(() => {
           console.log("not collecting");
-          state = "waiting";
+          state = WAITING;
         }, 60000);
       }, 10000);
     }
   };
 
   const setup = (p5) => {
-    p5.createCanvas(320, 240);
+    p5.createCanvas(VIDEO_WIDTH, VIDEO_HEIGHT);
     p5.background(51);
-    video = p5.createCapture(videoConstraints);
+    video = p5.createCapture(VIDEO_SETTINGS);
     video.position(0, 0);
     video.hide();
-    const playButton = p5.createButton("Play/ Reset");
-    const debugButton = p5.createButton("Debug Mode");
+    createButtonGroup(p5);
+    p5.noLoop();
+  };
+
+  const createButtonGroup = (p5) => {
+    const buttonGroup = p5.createDiv();
+    buttonGroup.style("display", "flex");
+    buttonGroup.style("direction", "row");
+    const playButton = p5.createButton("Play | Reset");
+    const debugButton = p5.createButton("Debug");
     const trainButton = p5.createButton("Train");
-    playButton.mousePressed(() => {
-      if (!running) {
-        poseNet = ml5.poseNet(video, modelLoaded);
-        poseNet.on("pose", gotPoses);
-        let options = {
-          inputs: 34,
-          outputs: 5,
-          task: "classification",
-          debug: true,
-        };
-        neurons = ml5.neuralNetwork(options);
-        const modelSpecs = {
-          model: "model4/model.json",
-          metadata: "model4/model_meta.json",
-          weights: "model4/model.weights.bin",
-        };
-        neurons.load(modelSpecs, neuronsLoaded);
-        running = true;
-        p5.loop();
-      } else {
-        running = false;
-        timer = totalGameTime;
-        p5.background(51);
-        if (poseNet) poseNet.removeListener("pose", gotPoses);
-      }
-    });
+    playButton.parent(buttonGroup);
+    debugButton.parent(buttonGroup);
+    trainButton.parent(buttonGroup);
+    playButton.mousePressed(() => onPlayButtonClick(p5));
     debugButton.mousePressed(() => (debugMode = !debugMode));
-    trainButton.mousePressed(() => {
+    trainButton.mousePressed(() => onTrainButtonClick());
+  };
+
+  const onPlayButtonClick = (p5) => {
+    if (!running) {
+      poseNet = ml5.poseNet(video);
+      poseNet.on("pose", getData);
       let options = {
         inputs: 34,
         outputs: 5,
         task: "classification",
         debug: true,
       };
-      neurons = ml5.neuralNetwork(options);
-      neurons.loadData("last.json", dataReady);
-    });
+      neuralNetwork = ml5.neuralNetwork(options);
+      const modelSpecs = {
+        model: "model4/model.json",
+        metadata: "model4/model_meta.json",
+        weights: "model4/model.weights.bin",
+      };
+      neuralNetwork?.load(modelSpecs, classifyData);
+      running = true;
+      p5.loop();
+    } else {
+      running = false;
+      timer = TOTAL_GAME_TIME;
+      poseNet?.removeListener("pose", getData);
+      p5.background(51);
+      p5.noLoop();
+    }
   };
 
-  const neuronsLoaded = () => {
-    console.log("neurons ready");
-
-    classifyPose();
+  const onTrainButtonClick = () => {
+    let options = {
+      inputs: 34,
+      outputs: 5,
+      task: "classification",
+      debug: true,
+    };
+    neuralNetwork = ml5.neuralNetwork(options);
+    neuralNetwork.loadData("last.json", train);
   };
 
-  const classifyPose = () => {
+  const classifyData = () => {
     if (pose && pose.keypoints && running && timer !== 0) {
       let inputs = [];
       for (let i = 0; i < pose.keypoints.length; i++) {
@@ -108,15 +126,15 @@ const App = () => {
         inputs.push(x);
         inputs.push(y);
       }
-      neurons.classify(inputs, gotResult);
+      neuralNetwork.classify(inputs, getLabel);
     } else if (!running || timer === 0) {
       return;
     } else {
-      setTimeout(classifyPose, 100);
+      setTimeout(classifyData, 100);
     }
   };
 
-  const gotResult = (results) => {
+  const getLabel = (error, results) => {
     if (results !== undefined && results.lenght !== 0) {
       sequence.push(results[0].label);
     }
@@ -124,32 +142,35 @@ const App = () => {
       if (mostCommon(sequence) === randomPose) {
         cool = true; // turn poseLabel green
         movementCount++;
-        randomPose =
-          possiblePoses[Math.floor(Math.random() * possiblePoses.length)];
+        randomPose = POSSIBLE_POSES.filter((pose) => pose !== randomPose)[
+          Math.floor(
+            Math.random() *
+              POSSIBLE_POSES.filter((pose) => pose !== randomPose).length
+          )
+        ];
       }
       sequence = [];
     }
-    classifyPose();
+    classifyData();
   };
 
-  const dataReady = () => {
-    neurons.normalizeData();
-    neurons.train({ epochs: 100 }, finished);
+  const train = () => {
+    neuralNetwork.normalizeData();
+    neuralNetwork.train({ epochs: 100 }, saveModel);
   };
 
-  const finished = () => {
-    console.log("model finished training");
-    neurons.save();
+  const saveModel = () => {
+    neuralNetwork.save();
   };
 
-  const gotPoses = (poses) => {
+  const getData = (poses) => {
     if (poses.length === 0) {
       pose = {};
     }
     if (poses.length > 0) {
       pose = poses[0].pose;
       skeleton = poses[0].skeleton;
-      if (state === "collecting") {
+      if (state === COLLECTING) {
         let inputs = [];
         for (let i = 0; i < pose.keypoints.length; i++) {
           let x = pose.keypoints[i].position.x;
@@ -158,16 +179,49 @@ const App = () => {
           inputs.push(y);
         }
         let target = [targetLabel];
-        neurons.addData(inputs, target);
+        neuralNetwork.addData(inputs, target);
       }
     }
   };
 
-  const modelLoaded = () => {
-    console.log("poseNet ready");
+  const draw = (p5) => {
+    if (running) {
+      p5.push();
+      p5.translate(VIDEO_WIDTH, 0);
+      p5.scale(-1, 1);
+      p5.image(video, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
+      if (pose !== undefined && pose.keypoints) {
+        debugMode && drawSkeleton(p5, pose.keypoints);
+        p5.pop();
+        if (cool) {
+          p5.fill(0, 255, 0);
+          //to do delay this a bit more
+          cool = false;
+        } else {
+          p5.fill(255, 0, 255);
+        }
+        p5.noStroke();
+        p5.textSize(100);
+        p5.textAlign("CENTER", "CENTER");
+        p5.text(randomPose, VIDEO_WIDTH / 2 + 100, VIDEO_HEIGHT / 2 - 60);
+        p5.textSize(40);
+        p5.text(timer, VIDEO_WIDTH / 2 + 105, VIDEO_HEIGHT / 2 - 20);
+        p5.ellipse(VIDEO_WIDTH / 2, VIDEO_HEIGHT / 2 - 30, 40, 40);
+        p5.frameCount % 60 === 0 && timer > 0 && timer--;
+      }
+      if (timer === 0) {
+        running = false;
+        timer = TOTAL_GAME_TIME;
+        poseNet?.removeListener("pose", getData);
+        p5.background(51);
+        p5.text(movementCount, VIDEO_WIDTH / 2 + 100, VIDEO_HEIGHT / 2 - 60);
+        p5.noLoop();
+        movementCount = 0;
+      }
+    }
   };
 
-  const debugPose = (p5, keypoints) => {
+  const drawSkeleton = (p5, keypoints) => {
     for (let i = 0; i < keypoints.length; i++) {
       let x = keypoints[i].position.x;
       let y = keypoints[i].position.y;
@@ -181,49 +235,6 @@ const App = () => {
       p5.strokeWeight(2);
       p5.stroke(255);
       p5.line(a.position.x, a.position.y, b.position.x, b.position.y);
-    }
-  };
-
-  const draw = (p5) => {
-    if (running) {
-      p5.push();
-      p5.translate(video.width, 0);
-      p5.scale(-1, 1);
-      p5.image(video, 0, 0, 320, 240);
-
-      if (pose !== undefined && pose.keypoints) {
-        if (debugMode) {
-          debugPose(p5, pose.keypoints);
-        }
-        p5.pop();
-        if (cool) {
-          p5.fill(0, 255, 0);
-          //to do delay this a bit more
-          cool = false;
-        } else {
-          p5.fill(255, 0, 255);
-        }
-        p5.noStroke();
-        p5.textSize(100);
-        p5.textAlign("CENTER", "CENTER");
-        p5.text(randomPose, video.width / 2 + 100, video.height / 2 - 60);
-        p5.textSize(40);
-        p5.text(timer, video.width / 2 + 105, video.height / 2 - 20);
-        p5.ellipse(video.width / 2, video.height / 2 - 30, 40, 40);
-        if (p5.frameCount % 60 === 0 && timer > 0) {
-          timer--;
-        }
-      }
-      if (!running || timer === 0) {
-        running = false;
-        timer = totalGameTime;
-        if (poseNet) {
-          poseNet.removeListener("pose", gotPoses);
-        }
-        p5.background(51);
-        p5.text(movementCount, video.width / 2 + 100, video.height / 2 - 60);
-        p5.noLoop();
-      }
     }
   };
 
---
+++ my-app/src/constants.js
@@ -0,0 +1,7 @@
+export const constants = {
+  TOTAL_GAME_TIME: 30,
+  VIDEO_HEIGHT: 240,
+  VIDEO_WIDTH: 320,
+  POSSIBLE_POSES: ["a", "b", "c", "e"],
+  STATE: { WAITING: "waiting", COLLECTING: "collecting" },
+};
